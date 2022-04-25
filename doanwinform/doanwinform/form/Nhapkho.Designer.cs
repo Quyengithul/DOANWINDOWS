0fbbb60f41ff0f96d0546df67acc5c46e466410a
doduyhao2002@gmail.com <doduyhao2002@gmail.com>
2022-04-25 20:21:16 +0700

    nhapkho va dangnhap

--- doanwinform/doanwinform/form/Nhapkho.Designer.cs
+++ doanwinform/doanwinform/form/Nhapkho.Designer.cs
@@ -29,19 +29,344 @@ namespace doanwinform.form
         /// </summary>
         private void InitializeComponent()
         {
+            this.cbb_don_vi = new System.Windows.Forms.ComboBox();
+            this.cbb_loai = new System.Windows.Forms.ComboBox();
+            this.dgv_nhap_kho = new System.Windows.Forms.DataGridView();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.txb_search = new System.Windows.Forms.TextBox();
+            this.btn_chon_anh = new System.Windows.Forms.Button();
+            this.b = new System.Windows.Forms.Button();
+            this.pictureBox_anh = new System.Windows.Forms.PictureBox();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.txb_ma_khuyen_mai = new System.Windows.Forms.TextBox();
+            this.txb_gia_ban = new System.Windows.Forms.TextBox();
+            this.txb_gia_nhap = new System.Windows.Forms.TextBox();
+            this.txb_so_luong = new System.Windows.Forms.TextBox();
+            this.txb_ten_san_pham = new System.Windows.Forms.TextBox();
+            this.txb_ma_san_pham = new System.Windows.Forms.TextBox();
+            this.lb_ma_khuyen_mai = new System.Windows.Forms.Label();
+            this.lb_don_vi = new System.Windows.Forms.Label();
+            this.lb_loai = new System.Windows.Forms.Label();
+            this.lb_gia_ban = new System.Windows.Forms.Label();
+            this.lb_gia_nhap = new System.Windows.Forms.Label();
+            this.lb_so_luong = new System.Windows.Forms.Label();
+            this.lb_ten_san_pham = new System.Windows.Forms.Label();
+            this.lb_ma_san_pham = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_nhap_kho)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_anh)).BeginInit();
             this.SuspendLayout();
+            // 
+            // cbb_don_vi
+            // 
+            this.cbb_don_vi.FormattingEnabled = true;
+            this.cbb_don_vi.Location = new System.Drawing.Point(146, 238);
+            this.cbb_don_vi.Name = "cbb_don_vi";
+            this.cbb_don_vi.Size = new System.Drawing.Size(192, 24);
+            this.cbb_don_vi.TabIndex = 81;
+            // 
+            // cbb_loai
+            // 
+            this.cbb_loai.FormattingEnabled = true;
+            this.cbb_loai.Location = new System.Drawing.Point(146, 201);
+            this.cbb_loai.Name = "cbb_loai";
+            this.cbb_loai.Size = new System.Drawing.Size(192, 24);
+            this.cbb_loai.TabIndex = 80;
+            // 
+            // dgv_nhap_kho
+            // 
+            this.dgv_nhap_kho.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_nhap_kho.Location = new System.Drawing.Point(355, 101);
+            this.dgv_nhap_kho.Name = "dgv_nhap_kho";
+            this.dgv_nhap_kho.RowHeadersWidth = 51;
+            this.dgv_nhap_kho.RowTemplate.Height = 24;
+            this.dgv_nhap_kho.Size = new System.Drawing.Size(671, 404);
+            this.dgv_nhap_kho.TabIndex = 79;
+            // 
+            // btn_search
+            // 
+            this.btn_search.Location = new System.Drawing.Point(599, 61);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(84, 26);
+            this.btn_search.TabIndex = 78;
+            this.btn_search.Text = "Tìm kiếm";
+            this.btn_search.UseVisualStyleBackColor = true;
+            // 
+            // txb_search
+            // 
+            this.txb_search.Location = new System.Drawing.Point(355, 62);
+            this.txb_search.Name = "txb_search";
+            this.txb_search.Size = new System.Drawing.Size(238, 22);
+            this.txb_search.TabIndex = 77;
+            this.txb_search.Text = "Tìm kiếm";
+            // 
+            // btn_chon_anh
+            // 
+            this.btn_chon_anh.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btn_chon_anh.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_chon_anh.Location = new System.Drawing.Point(243, 427);
+            this.btn_chon_anh.Name = "btn_chon_anh";
+            this.btn_chon_anh.Size = new System.Drawing.Size(95, 42);
+            this.btn_chon_anh.TabIndex = 76;
+            this.btn_chon_anh.Text = "Chọn ảnh";
+            this.btn_chon_anh.UseVisualStyleBackColor = false;
+            // 
+            // b
+            // 
+            this.b.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.b.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b.Location = new System.Drawing.Point(243, 366);
+            this.b.Name = "b";
+            this.b.Size = new System.Drawing.Size(95, 42);
+            this.b.TabIndex = 75;
+            this.b.Text = "Xoá ảnh";
+            this.b.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox_anh
+            // 
+            this.pictureBox_anh.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.pictureBox_anh.Location = new System.Drawing.Point(15, 366);
+            this.pictureBox_anh.Name = "pictureBox_anh";
+            this.pictureBox_anh.Size = new System.Drawing.Size(212, 139);
+            this.pictureBox_anh.TabIndex = 74;
+            this.pictureBox_anh.TabStop = false;
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btn_clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clear.Location = new System.Drawing.Point(263, 309);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(75, 32);
+            this.btn_clear.TabIndex = 73;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = false;
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btn_delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_delete.Location = new System.Drawing.Point(180, 310);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(75, 31);
+            this.btn_delete.TabIndex = 72;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = false;
+            // 
+            // btn_save
+            // 
+            this.btn_save.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btn_save.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_save.Location = new System.Drawing.Point(97, 309);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(75, 32);
+            this.btn_save.TabIndex = 71;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = false;
+            // 
+            // btn_add
+            // 
+            this.btn_add.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btn_add.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_add.Location = new System.Drawing.Point(15, 310);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(75, 31);
+            this.btn_add.TabIndex = 70;
+            this.btn_add.Text = "Add";
+            this.btn_add.UseVisualStyleBackColor = false;
+            // 
+            // txb_ma_khuyen_mai
+            // 
+            this.txb_ma_khuyen_mai.Location = new System.Drawing.Point(146, 268);
+            this.txb_ma_khuyen_mai.Name = "txb_ma_khuyen_mai";
+            this.txb_ma_khuyen_mai.Size = new System.Drawing.Size(192, 22);
+            this.txb_ma_khuyen_mai.TabIndex = 69;
+            // 
+            // txb_gia_ban
+            // 
+            this.txb_gia_ban.Location = new System.Drawing.Point(146, 170);
+            this.txb_gia_ban.Name = "txb_gia_ban";
+            this.txb_gia_ban.Size = new System.Drawing.Size(192, 22);
+            this.txb_gia_ban.TabIndex = 68;
+            // 
+            // txb_gia_nhap
+            // 
+            this.txb_gia_nhap.Location = new System.Drawing.Point(146, 135);
+            this.txb_gia_nhap.Name = "txb_gia_nhap";
+            this.txb_gia_nhap.Size = new System.Drawing.Size(192, 22);
+            this.txb_gia_nhap.TabIndex = 67;
+            // 
+            // txb_so_luong
+            // 
+            this.txb_so_luong.Location = new System.Drawing.Point(146, 101);
+            this.txb_so_luong.Name = "txb_so_luong";
+            this.txb_so_luong.Size = new System.Drawing.Size(192, 22);
+            this.txb_so_luong.TabIndex = 66;
+            // 
+            // txb_ten_san_pham
+            // 
+            this.txb_ten_san_pham.Location = new System.Drawing.Point(146, 65);
+            this.txb_ten_san_pham.Name = "txb_ten_san_pham";
+            this.txb_ten_san_pham.Size = new System.Drawing.Size(192, 22);
+            this.txb_ten_san_pham.TabIndex = 65;
+            // 
+            // txb_ma_san_pham
+            // 
+            this.txb_ma_san_pham.Location = new System.Drawing.Point(146, 32);
+            this.txb_ma_san_pham.Name = "txb_ma_san_pham";
+            this.txb_ma_san_pham.Size = new System.Drawing.Size(192, 22);
+            this.txb_ma_san_pham.TabIndex = 64;
+            // 
+            // lb_ma_khuyen_mai
+            // 
+            this.lb_ma_khuyen_mai.AutoSize = true;
+            this.lb_ma_khuyen_mai.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_ma_khuyen_mai.Location = new System.Drawing.Point(12, 267);
+            this.lb_ma_khuyen_mai.Name = "lb_ma_khuyen_mai";
+            this.lb_ma_khuyen_mai.Size = new System.Drawing.Size(121, 20);
+            this.lb_ma_khuyen_mai.TabIndex = 63;
+            this.lb_ma_khuyen_mai.Text = "Mã khuyến mãi";
+            // 
+            // lb_don_vi
+            // 
+            this.lb_don_vi.AutoSize = true;
+            this.lb_don_vi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_don_vi.Location = new System.Drawing.Point(12, 237);
+            this.lb_don_vi.Name = "lb_don_vi";
+            this.lb_don_vi.Size = new System.Drawing.Size(56, 20);
+            this.lb_don_vi.TabIndex = 62;
+            this.lb_don_vi.Text = "Đơn vị";
+            // 
+            // lb_loai
+            // 
+            this.lb_loai.AutoSize = true;
+            this.lb_loai.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_loai.Location = new System.Drawing.Point(12, 201);
+            this.lb_loai.Name = "lb_loai";
+            this.lb_loai.Size = new System.Drawing.Size(41, 20);
+            this.lb_loai.TabIndex = 61;
+            this.lb_loai.Text = "Loại";
+            // 
+            // lb_gia_ban
+            // 
+            this.lb_gia_ban.AutoSize = true;
+            this.lb_gia_ban.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_gia_ban.Location = new System.Drawing.Point(12, 172);
+            this.lb_gia_ban.Name = "lb_gia_ban";
+            this.lb_gia_ban.Size = new System.Drawing.Size(67, 20);
+            this.lb_gia_ban.TabIndex = 60;
+            this.lb_gia_ban.Text = "Giá bán";
+            // 
+            // lb_gia_nhap
+            // 
+            this.lb_gia_nhap.AutoSize = true;
+            this.lb_gia_nhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_gia_nhap.Location = new System.Drawing.Point(12, 136);
+            this.lb_gia_nhap.Name = "lb_gia_nhap";
+            this.lb_gia_nhap.Size = new System.Drawing.Size(76, 20);
+            this.lb_gia_nhap.TabIndex = 59;
+            this.lb_gia_nhap.Text = "Giá nhập";
+            // 
+            // lb_so_luong
+            // 
+            this.lb_so_luong.AutoSize = true;
+            this.lb_so_luong.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_so_luong.Location = new System.Drawing.Point(12, 101);
+            this.lb_so_luong.Name = "lb_so_luong";
+            this.lb_so_luong.Size = new System.Drawing.Size(74, 20);
+            this.lb_so_luong.TabIndex = 58;
+            this.lb_so_luong.Text = "Số lượng";
+            // 
+            // lb_ten_san_pham
+            // 
+            this.lb_ten_san_pham.AutoSize = true;
+            this.lb_ten_san_pham.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_ten_san_pham.Location = new System.Drawing.Point(12, 67);
+            this.lb_ten_san_pham.Name = "lb_ten_san_pham";
+            this.lb_ten_san_pham.Size = new System.Drawing.Size(115, 20);
+            this.lb_ten_san_pham.TabIndex = 57;
+            this.lb_ten_san_pham.Text = "Tên sản phẩm";
+            // 
+            // lb_ma_san_pham
+            // 
+            this.lb_ma_san_pham.AutoSize = true;
+            this.lb_ma_san_pham.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_ma_san_pham.Location = new System.Drawing.Point(12, 34);
+            this.lb_ma_san_pham.Name = "lb_ma_san_pham";
+            this.lb_ma_san_pham.Size = new System.Drawing.Size(110, 20);
+            this.lb_ma_san_pham.TabIndex = 56;
+            this.lb_ma_san_pham.Text = "Mã sản phẩm";
             // 
             // Nhapkho
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(1039, 536);
+            this.Controls.Add(this.cbb_don_vi);
+            this.Controls.Add(this.cbb_loai);
+            this.Controls.Add(this.dgv_nhap_kho);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.txb_search);
+            this.Controls.Add(this.btn_chon_anh);
+            this.Controls.Add(this.b);
+            this.Controls.Add(this.pictureBox_anh);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.txb_ma_khuyen_mai);
+            this.Controls.Add(this.txb_gia_ban);
+            this.Controls.Add(this.txb_gia_nhap);
+            this.Controls.Add(this.txb_so_luong);
+            this.Controls.Add(this.txb_ten_san_pham);
+            this.Controls.Add(this.txb_ma_san_pham);
+            this.Controls.Add(this.lb_ma_khuyen_mai);
+            this.Controls.Add(this.lb_don_vi);
+            this.Controls.Add(this.lb_loai);
+            this.Controls.Add(this.lb_gia_ban);
+            this.Controls.Add(this.lb_gia_nhap);
+            this.Controls.Add(this.lb_so_luong);
+            this.Controls.Add(this.lb_ten_san_pham);
+            this.Controls.Add(this.lb_ma_san_pham);
             this.Name = "Nhapkho";
             this.Text = "Nhập kho";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_nhap_kho)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_anh)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cbb_don_vi;
+        private System.Windows.Forms.ComboBox cbb_loai;
+        private System.Windows.Forms.DataGridView dgv_nhap_kho;
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.TextBox txb_search;
+        private System.Windows.Forms.Button btn_chon_anh;
+        private System.Windows.Forms.Button b;
+        private System.Windows.Forms.PictureBox pictureBox_anh;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.TextBox txb_ma_khuyen_mai;
+        private System.Windows.Forms.TextBox txb_gia_ban;
+        private System.Windows.Forms.TextBox txb_gia_nhap;
+        private System.Windows.Forms.TextBox txb_so_luong;
+        private System.Windows.Forms.TextBox txb_ten_san_pham;
+        private System.Windows.Forms.TextBox txb_ma_san_pham;
+        private System.Windows.Forms.Label lb_ma_khuyen_mai;
+        private System.Windows.Forms.Label lb_don_vi;
+        private System.Windows.Forms.Label lb_loai;
+        private System.Windows.Forms.Label lb_gia_ban;
+        private System.Windows.Forms.Label lb_gia_nhap;
+        private System.Windows.Forms.Label lb_so_luong;
+        private System.Windows.Forms.Label lb_ten_san_pham;
+        private System.Windows.Forms.Label lb_ma_san_pham;
     }
 }

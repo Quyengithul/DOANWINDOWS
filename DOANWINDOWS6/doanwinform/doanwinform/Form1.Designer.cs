a963e5a27843b32a22578b202f3419541e812523
Quyen01022002 <102469677+Quyen01022002@users.noreply.github.com>
2022-05-02 20:44:49 +0700

    Add files via upload

---
+++ DOANWINDOWS6/doanwinform/doanwinform/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace doanwinform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.panellogo = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_Donhang = new System.Windows.Forms.Button();
+            this.btn_Nhapkho = new System.Windows.Forms.Button();
+            this.btn_tonkho = new System.Windows.Forms.Button();
+            this.btn_thongke = new System.Windows.Forms.Button();
+            this.btn_sanpham = new System.Windows.Forms.Button();
+            this.btn_dx = new System.Windows.Forms.Button();
+            this.panelTitle = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.paneltrangchu = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.panelTitle.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.panelMenu.Controls.Add(this.btn_dx);
+            this.panelMenu.Controls.Add(this.btn_sanpham);
+            this.panelMenu.Controls.Add(this.btn_thongke);
+            this.panelMenu.Controls.Add(this.btn_tonkho);
+            this.panelMenu.Controls.Add(this.btn_Nhapkho);
+            this.panelMenu.Controls.Add(this.btn_Donhang);
+            this.panelMenu.Controls.Add(this.button1);
+            this.panelMenu.Controls.Add(this.panellogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(200, 601);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // panellogo
+            // 
+            this.panellogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(76)))));
+            this.panellogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panellogo.Location = new System.Drawing.Point(0, 0);
+            this.panellogo.Name = "panellogo";
+            this.panellogo.Size = new System.Drawing.Size(200, 80);
+            this.panellogo.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(0, 80);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.button1.Size = new System.Drawing.Size(200, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Trang chủ";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_Donhang
+            // 
+            this.btn_Donhang.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_Donhang.FlatAppearance.BorderSize = 0;
+            this.btn_Donhang.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Donhang.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Donhang.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_Donhang.Image = ((System.Drawing.Image)(resources.GetObject("btn_Donhang.Image")));
+            this.btn_Donhang.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Donhang.Location = new System.Drawing.Point(0, 140);
+            this.btn_Donhang.Name = "btn_Donhang";
+            this.btn_Donhang.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btn_Donhang.Size = new System.Drawing.Size(200, 60);
+            this.btn_Donhang.TabIndex = 1;
+            this.btn_Donhang.Text = "Đơn hàng";
+            this.btn_Donhang.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_Donhang.UseVisualStyleBackColor = true;
+            this.btn_Donhang.Click += new System.EventHandler(this.btn_Donhang_Click);
+            // 
+            // btn_Nhapkho
+            // 
+            this.btn_Nhapkho.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_Nhapkho.FlatAppearance.BorderSize = 0;
+            this.btn_Nhapkho.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Nhapkho.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Nhapkho.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_Nhapkho.Image = ((System.Drawing.Image)(resources.GetObject("btn_Nhapkho.Image")));
+            this.btn_Nhapkho.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Nhapkho.Location = new System.Drawing.Point(0, 200);
+            this.btn_Nhapkho.Name = "btn_Nhapkho";
+            this.btn_Nhapkho.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btn_Nhapkho.Size = new System.Drawing.Size(200, 60);
+            this.btn_Nhapkho.TabIndex = 2;
+            this.btn_Nhapkho.Text = "Nhập kho";
+            this.btn_Nhapkho.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_Nhapkho.UseVisualStyleBackColor = true;
+            this.btn_Nhapkho.Click += new System.EventHandler(this.btn_Nhapkho_Click);
+            // 
+            // btn_tonkho
+            // 
+            this.btn_tonkho.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_tonkho.FlatAppearance.BorderSize = 0;
+            this.btn_tonkho.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_tonkho.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_tonkho.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_tonkho.Image = ((System.Drawing.Image)(resources.GetObject("btn_tonkho.Image")));
+            this.btn_tonkho.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_tonkho.Location = new System.Drawing.Point(0, 260);
+            this.btn_tonkho.Name = "btn_tonkho";
+            this.btn_tonkho.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btn_tonkho.Size = new System.Drawing.Size(200, 60);
+            this.btn_tonkho.TabIndex = 3;
+            this.btn_tonkho.Text = "Tồn kho";
+            this.btn_tonkho.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_tonkho.UseVisualStyleBackColor = true;
+            this.btn_tonkho.Click += new System.EventHandler(this.btn_tonkho_Click);
+            // 
+            // btn_thongke
+            // 
+            this.btn_thongke.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_thongke.FlatAppearance.BorderSize = 0;
+            this.btn_thongke.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_thongke.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_thongke.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_thongke.Image = ((System.Drawing.Image)(resources.GetObject("btn_thongke.Image")));
+            this.btn_thongke.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_thongke.Location = new System.Drawing.Point(0, 320);
+            this.btn_thongke.Name = "btn_thongke";
+            this.btn_thongke.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btn_thongke.Size = new System.Drawing.Size(200, 60);
+            this.btn_thongke.TabIndex = 4;
+            this.btn_thongke.Text = "Thống kê";
+            this.btn_thongke.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_thongke.UseVisualStyleBackColor = true;
+            this.btn_thongke.Click += new System.EventHandler(this.btn_thongke_Click);
+            // 
+            // btn_sanpham
+            // 
+            this.btn_sanpham.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_sanpham.FlatAppearance.BorderSize = 0;
+            this.btn_sanpham.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_sanpham.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_sanpham.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_sanpham.Image = ((System.Drawing.Image)(resources.GetObject("btn_sanpham.Image")));
+            this.btn_sanpham.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_sanpham.Location = new System.Drawing.Point(0, 380);
+            this.btn_sanpham.Name = "btn_sanpham";
+            this.btn_sanpham.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btn_sanpham.Size = new System.Drawing.Size(200, 60);
+            this.btn_sanpham.TabIndex = 5;
+            this.btn_sanpham.Text = "Sản phẩm";
+            this.btn_sanpham.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_sanpham.UseVisualStyleBackColor = true;
+            this.btn_sanpham.Click += new System.EventHandler(this.btn_sanpham_Click);
+            // 
+            // btn_dx
+            // 
+            this.btn_dx.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btn_dx.FlatAppearance.BorderSize = 0;
+            this.btn_dx.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_dx.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_dx.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_dx.Image = ((System.Drawing.Image)(resources.GetObject("btn_dx.Image")));
+            this.btn_dx.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_dx.Location = new System.Drawing.Point(0, 541);
+            this.btn_dx.Name = "btn_dx";
+            this.btn_dx.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.btn_dx.Size = new System.Drawing.Size(200, 60);
+            this.btn_dx.TabIndex = 6;
+            this.btn_dx.Text = "Đăng xuất";
+            this.btn_dx.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_dx.UseVisualStyleBackColor = true;
+            // 
+            // panelTitle
+            // 
+            this.panelTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
+            this.panelTitle.Controls.Add(this.label1);
+            this.panelTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitle.Location = new System.Drawing.Point(200, 0);
+            this.panelTitle.Name = "panelTitle";
+            this.panelTitle.Size = new System.Drawing.Size(1050, 80);
+            this.panelTitle.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(460, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Trang chủ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // paneltrangchu
+            // 
+            this.paneltrangchu.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.paneltrangchu.Location = new System.Drawing.Point(200, 80);
+            this.paneltrangchu.Name = "paneltrangchu";
+            this.paneltrangchu.Size = new System.Drawing.Size(1050, 521);
+            this.paneltrangchu.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1250, 601);
+            this.Controls.Add(this.paneltrangchu);
+            this.Controls.Add(this.panelTitle);
+            this.Controls.Add(this.panelMenu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelMenu.ResumeLayout(false);
+            this.panelTitle.ResumeLayout(false);
+            this.panelTitle.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button btn_Donhang;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panellogo;
+        private System.Windows.Forms.Button btn_dx;
+        private System.Windows.Forms.Button btn_sanpham;
+        private System.Windows.Forms.Button btn_thongke;
+        private System.Windows.Forms.Button btn_tonkho;
+        private System.Windows.Forms.Button btn_Nhapkho;
+        private System.Windows.Forms.Panel panelTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel paneltrangchu;
+    }
+}
+
